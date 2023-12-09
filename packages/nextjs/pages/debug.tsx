a1ba80382ecf73cdc24b842aef54e5ea8f02ac77
Aryan Godara <aryangodara03@gmail.com>
2023-12-09 17:00:48 +0530

    initial commit, update scafold to filecoin contract

---
+++ packages/nextjs/pages/debug.tsx
@@ -0,0 +1,115 @@
+import { useEffect } from "react";
+import type { NextPage } from "next";
+import { useLocalStorage } from "usehooks-ts";
+import { MetaHeader } from "~~/components/MetaHeader";
+import { ContractUI } from "~~/components/scaffold-eth";
+import { ContractName } from "~~/utils/scaffold-eth/contract";
+import { getContractNames } from "~~/utils/scaffold-eth/contractNames";
+
+import React, { useState } from 'react';
+
+const selectedContractStorageKey = "scaffoldEth2.selectedContract";
+const contractNames = getContractNames();
+
+const Debug: NextPage = () => {
+  const [selectedContract, setSelectedContract] = useLocalStorage<ContractName>(
+    selectedContractStorageKey,
+    contractNames[0],
+  );
+
+  const [selectedFileName, setSelectedFileName] = useState('')
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+
+  useEffect(() => {
+    if (!contractNames.includes(selectedContract)) {
+      setSelectedContract(contractNames[0]);
+    }
+  }, [selectedContract, setSelectedContract]);
+
+  return (
+    <>
+      <MetaHeader
+        title="Debug Contracts | Scaffold-ETH 2"
+        description="Debug your deployed 🏗 Scaffold-ETH 2 contracts in an easy way"
+      />
+      {/* To Upload Files */}
+      <div className="flex flex-col gap-y-6 lg:gap-y-8 py-8 lg:py-12 justify-center items-center">
+        
+      <input type="file" onChange={(event) => {
+        const file = event.target.files?.item(0);
+        if (file) {
+          console.log(file);
+          setSelectedFile(file);
+          setSelectedFileName(file.name);
+        } else {
+          console.log('No file selected');
+        }
+      }} />
+
+      <p>Selected file: {selectedFileName}</p>
+
+      <button onClick={async () => {
+        if (selectedFileName) {
+          const formData = new FormData();
+            formData.append('file', selectedFile);
+
+          const response = await fetch('https://your-server.com/upload', {
+            method: 'POST',
+            body: formData,
+          });
+
+          if (response.ok) {
+            console.log('File uploaded successfully');
+          } else {
+            console.log('File upload failed');
+          }
+        }
+      }}>Upload</button>
+      </div>
+
+      <div className="flex flex-col gap-y-6 lg:gap-y-8 py-8 lg:py-12 justify-center items-center">
+        {contractNames.length === 0 ? (
+          <p className="text-3xl mt-14">No contracts found!</p>
+        ) : (
+          <>
+            {contractNames.length > 1 && (
+              <div className="flex flex-row gap-2 w-full max-w-7xl pb-1 px-6 lg:px-10 flex-wrap">
+                {contractNames.map(contractName => (
+                  <button
+                    className={`btn btn-secondary btn-sm normal-case font-thin ${
+                      contractName === selectedContract ? "bg-base-300" : "bg-base-100"
+                    }`}
+                    key={contractName}
+                    onClick={() => setSelectedContract(contractName)}
+                  >
+                    {contractName}
+                  </button>
+                ))}
+              </div>
+            )}
+            {contractNames.map(contractName => (
+              <ContractUI
+                key={contractName}
+                contractName={contractName}
+                className={contractName === selectedContract ? "" : "hidden"}
+              />
+            ))}
+          </>
+        )}
+      </div>
+      <div className="text-center mt-8 bg-secondary p-10">
+        <h1 className="text-4xl my-0">Debug Contracts</h1>
+        <p className="text-neutral">
+          You can debug & interact with your deployed contracts here.
+          <br /> Check{" "}
+          <code className="italic bg-base-300 text-base font-bold [word-spacing:-0.5rem] px-1">
+            packages / nextjs / pages / debug.tsx
+          </code>{" "}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Debug;
